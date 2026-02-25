80ffd491e770c98fed04a25ce91bf6e1ef022503
Nishan-k <karki.nishann@gmail.com>
2026-02-25 22:57:16 +0100

    data pushed as JSON to MongoDB

--- push_data.py
+++ push_data.py
@@ -10,13 +10,90 @@ from src.customer_churn.logging import logger
 from src.customer_churn.exception.exception import CustomerChurnException
 
 
-# 1. Read the xlsx file as a dataframe:
+# 1. Read the xlsx file as a dataframe and save as parquet file:
 sheet_names = ['Year 2009-2010', 'Year 2010-2011']
 df = pd.concat(
     pd.read_excel("data/online_retail_II.xlsx", sheet_name=sheet_names).values(),
     ignore_index=True
 )
 
+# Change the necessary dtypes:
+df['Invoice'] = df['Invoice'].astype('str')
+df['StockCode'] = df['StockCode'].astype('str')
+df['Description'] = df['Description'].astype('str')
 
 
+
+# 2. Load the .env file:
+load_dotenv()
+
+mongo_uri = os.getenv("MONGO_DB_URL")
+ca = certifi.where()
+
+
+class LoadRetailData():
+
+    def __init__(self):
+        pass
+    
+    def convert_xlsx_to_parquet_and_load(self, file_path, sheet_names):
+        """
+        This function reads the .xlsx file as Pandas DF, saves it as a Parquet file, reads the parquet file and
+        returns as Pandas DF:
+        """
+        self.file_path = file_path
+        self.sheet_names = sheet_names
+        df = pd.concat(pd.read_excel(self.file_path, sheet_name=self.sheet_names).values(),
+                       ignore_index=True)
+        
+        # Chnage the necessary dtypes:
+        df['Invoice'] = df['Invoice'].astype('str')
+        df['StockCode'] = df['StockCode'].astype('str')
+        df['Description'] = df['Description'].astype('str')
+
+        # Save the DF as Parquet file:
+        df.to_parquet("data/online_retail.parquet")
+
+        # Read the Parquet file:
+        df = pd.read_parquet("data/online_retail.parquet")
+
+        return df
+
+    def parquest_data_to_json_converter(self, df):
+        self.df = df
+        try:
+            df.reset_index(drop=True, inplace=True)
+            # Convert the DF to json file:
+            records = list(json.loads(df.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise CustomerChurnException(e, sys)
+
+    def insert_data_to_mongodb(self, records, database, collection):
+        try:
+            self.records = records
+            self.database = database
+            self.collection = collection
+
+            self.mongo_client = pymongo.MongoClient(mongo_uri)
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return (len(self.records))
+        except Exception as e:
+            raise CustomerChurnException(e, sys)
+
+
+if __name__ == "__main__":
+    FILE_PATH = "data/online_retail_II.xlsx"
+    SHEET_NAMES = ['Year 2009-2010', 'Year 2010-2011']
+    DATABASE = "online_retail"
+    COLLECTION = "customer_data"
+    retail_obj = LoadRetailData()
+    parq_as_df = retail_obj.convert_xlsx_to_parquet_and_load(file_path=FILE_PATH, sheet_names=SHEET_NAMES)
+    data_as_json = retail_obj.parquest_data_to_json_converter(df=parq_as_df)
+    num_of_records = retail_obj.insert_data_to_mongodb(records=data_as_json, 
+                                                       database=DATABASE, 
+                                                       collection=COLLECTION)
+    print(f"A total of: {num_of_records} were inserted.")
 
---
+++ src/customer_churn/config/configuration.py
@@ -0,0 +1,6 @@
+
+
+class ConfiguraitonManager():
+    def __init__(self):
+        pass
+
